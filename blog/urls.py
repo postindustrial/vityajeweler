0be11eb088e59b5415e869ba94376456383b5eca
postindustrial <victorlegotin@gmail.com>
2019-04-18 13:26:30 +0500

    переписал две вьюхи на классы +верстка

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('blog/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.PostList.as_view()),
+    path('blog/<slug:slug>/', views.TaggedList.as_view()),
+#    path('blog/<slug:slug>/', views.post_detail, name='post_detail'),
     path('tag/<slug:slug>/', views.tagged_post_list, name='tagged_post_list'),
 ]
